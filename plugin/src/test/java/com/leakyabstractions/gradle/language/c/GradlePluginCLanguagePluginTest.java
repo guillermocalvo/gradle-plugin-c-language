f09634786a1c58b5522eea5f0f141233ca8049fc
Guillermo Calvo <guillermo@guillermo.dev>
2023-01-08 16:49:20 +0100

    Format source code

--- plugin/src/test/java/com/leakyabstractions/gradle/language/c/GradlePluginCLanguagePluginTest.java
+++ plugin/src/test/java/com/leakyabstractions/gradle/language/c/GradlePluginCLanguagePluginTest.java
@@ -1,16 +1,16 @@
 
 package com.leakyabstractions.gradle.language.c;
 
-import org.gradle.testfixtures.ProjectBuilder;
-import org.gradle.api.Project;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * A simple unit test for the 'com.leakyabstractions.gradle.language.c.greeting' plugin.
- */
+import org.gradle.api.Project;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.jupiter.api.Test;
+
+/** A simple unit test for the 'com.leakyabstractions.gradle.language.c.greeting' plugin. */
 class GradlePluginCLanguagePluginTest {
-    @Test void pluginRegistersATask() {
+    @Test
+    void pluginRegistersATask() {
         // Create a test project and apply the plugin
         Project project = ProjectBuilder.builder().build();
         project.getPlugins().apply("com.leakyabstractions.gradle.language.c.greeting");
