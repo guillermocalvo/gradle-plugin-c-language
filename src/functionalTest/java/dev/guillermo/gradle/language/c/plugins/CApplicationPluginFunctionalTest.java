43aa71734065f30589cd9a154f115552ea0a3f70
Guillermo Calvo <guillermo@guillermo.dev>
2023-01-14 14:09:51 +0100

    Add functional tests

---
+++ src/functionalTest/java/dev/guillermo/gradle/language/c/plugins/CApplicationPluginFunctionalTest.java
@@ -0,0 +1,38 @@
+
+package dev.guillermo.gradle.language.c.plugins;
+
+import static dev.guillermo.gradle.language.c.plugins.TestHelper.writeFile;
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class CApplicationPluginFunctionalTest {
+
+    @TempDir
+    File projectDir;
+
+    @Test
+    void buildApplication() throws IOException {
+        // Given
+        writeFile(projectDir, "settings.gradle", "rootProject.name = 'app'");
+        writeFile(
+                projectDir, "build.gradle", "plugins {", "  id('dev.guillermo.gradle.c-application')", "}");
+        writeFile(projectDir, "src/main/c/foo.c", "int main(void){", "  return 0;", "}");
+        // When
+        final BuildResult result = GradleRunner.create()
+                .forwardOutput()
+                .withPluginClasspath()
+                .withArguments("build")
+                .withProjectDir(projectDir)
+                .build();
+        // Then
+        assertEquals(SUCCESS, result.task(":build").getOutcome());
+    }
+}
